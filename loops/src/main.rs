b26f3c9d09ca12053ba3f4e3d2a0656810390de0
Juan Gonzalez <jrg2156@gmail.com>
2020-05-30 17:18:04 -0400

    loops

---
+++ loops/src/main.rs
@@ -0,0 +1,50 @@
+fn main() {
+    // infinite loop
+    infinite_loop();
+
+    // returning a value from a loop
+    value_from_loop();
+
+    // while loop
+    while_loop();
+
+    // for loop
+    for_loop();
+}
+
+fn infinite_loop() {
+    loop {
+        break;
+    }
+}
+
+fn value_from_loop() {
+    let mut counter = 0;
+
+    let _result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+}
+
+fn while_loop() {
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+
+        index += 1;
+    }
+}
+
+fn for_loop() {
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a.iter() {
+        println!("the value is: {}", element);
+    }
+}
